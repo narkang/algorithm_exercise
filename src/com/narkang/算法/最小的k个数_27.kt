2a3c60d056b0c71d19f5d53cff0d5804c26a4c0f
narkang <18658169731@163.com>
2019-01-11 13:44:00 +0800

    最小的k个数

--- src/com/narkang/算法/最小的k个数_27.kt
+++ src/com/narkang/算法/最小的k个数_27.kt
@@ -4,13 +4,13 @@ import java.util.*
 
 /**
  *  输入n个整数，找出其中最小的k个数。
+ *  https://blog.csdn.net/zhou15755387780/article/details/79163401
  */
 fun main(args: Array<String>) {
     var numbers = intArrayOf(4,5,1,6,2,7,3,8)
     var resultNumbers = getLeastNumbers(numbers, 4)
     printResultNumber(resultNumbers, 4)
 }
-
 
 fun printResultNumber(number: List<Int>?, k: Int){
     println("最小的${k}个数结果是：")
