65f4e4d96c4b2a34c37af1be8b3fdce9ce4985dc
narkang <18658169731@163.com>
2019-02-16 23:13:52 +0800

    约瑟夫环问题

--- algorithm/src/com/narkang/算法/剑指offer/约瑟夫环问题_45.kt
+++ algorithm/src/com/narkang/算法/剑指offer/约瑟夫环问题_45.kt
@@ -1,3 +1,30 @@
+/**
+ *  0, 1, … , n-1 这n个数字排成一个圈圈，从数字0开始每次从圆圏
+ *  里删除第m个数字。求出这个圈圈里剩下的最后一个数字。
+ */
 fun main(args: Array<String>) {
+    josephus(10, 2)
+}
 
+fun josephus(n: Int, m: Int){
+
+    var list = ArrayList<Int>(n)
+
+    for(i in 0 until n){
+        list.add(i)
+    }
+
+    var index = 0
+    while (list.size > 0){
+
+        index = (index + m) % list.size - 1
+        if(index < 0){
+            println(list[list.size - 1])
+            list.remove(list[list.size - 1])
+            index = 0
+        }else{
+            println(list[index])
+            list.remove(list[index])
+        }
+    }
 }
