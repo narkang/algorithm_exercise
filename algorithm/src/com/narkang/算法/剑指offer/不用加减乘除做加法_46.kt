cf6f331b4b76582b4f84f382f1924e11d470a672
narkang <18658169731@163.com>
2019-02-18 21:58:26 +0800

    不用加减乘除做加法

--- algorithm/src/com/narkang/算法/剑指offer/不用加减乘除做加法_46.kt
+++ algorithm/src/com/narkang/算法/剑指offer/不用加减乘除做加法_46.kt
@@ -2,5 +2,25 @@
  *  写一个函数，求两个整数之和，要求在函数体内不得使用＋、－、×、÷四则运算符号。
  */
 fun main(args: Array<String>) {
+    println(add(16, 17))
+}
+
+fun add(_n1: Int, _n2: Int): Int{
+
+    var n1 = _n1
+    var n2 = _n2
+    var sum: Int  //求和
+    var carry: Int //是否有二进制位
+
+    do{
+
+        sum = n1 xor n2
+        carry = (n1 and n2) shl 1
+        n1 = sum
+        n2 = carry
+
+    }while (carry != 0)
+
+    return sum
 
 }
