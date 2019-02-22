857eb7072646b52b2efb0faed30bdc5d0d905238
narkang <18658169731@163.com>
2019-02-22 11:07:49 +0800

    数组中重复的数字

--- algorithm/src/com/narkang/算法/剑指offer/数组中重复的数字_49.kt
+++ algorithm/src/com/narkang/算法/剑指offer/数组中重复的数字_49.kt
@@ -5,5 +5,30 @@
  * 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是重复的数字2或者3。
  */
 fun main(args: Array<String>) {
+    val number = intArrayOf(6,3,1,0,2,5,3)
+    val duplicateNumber = findDuplicatedNumber(number)
+    println("重复元素是$duplicateNumber")
+}
 
+fun findDuplicatedNumber(_number: IntArray): Int{
+
+    var number = _number
+    val length = _number.size
+
+    number.forEach {
+        if(it < 0 || it > length - 1)  throw IllegalArgumentException("Wrong Numbers in Array")
+    }
+
+    for(i in 0 until length){
+        while (number[i] != i){
+            if(number[i] == number[number[i]]){
+                return number[i]
+            }
+            val temp = number[i]
+            number[i] = number[temp]
+            number[temp] = temp
+        }
+    }
+
+    return -1
 }
