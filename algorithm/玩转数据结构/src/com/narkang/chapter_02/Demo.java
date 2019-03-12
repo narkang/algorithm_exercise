0d7ff8a8737a6288b4a74e8236cb6b6176fa6afd
narkang <18658169731@163.com>
2019-03-12 17:06:05 +0800

    测试Demo

--- algorithm/玩转数据结构/src/com/narkang/chapter_02/Demo.java
+++ algorithm/玩转数据结构/src/com/narkang/chapter_02/Demo.java
@@ -4,5 +4,27 @@ class Demo{
 
     public static void main(String[]args){
 
+        Array<Integer> data = new Array();
+
+        System.out.println(data.toString());
+        System.out.println(data.getCapacity());
+        System.out.println(data.getSize());
+
+        data.addFirst(1);
+        data.addFirst(2);
+        data.addFirst(3);
+        data.addFirst(4);
+
+        data.addLast(5);
+        data.addLast(6);
+
+        System.out.println(data.toString());
+        System.out.println(data.getCapacity());
+        System.out.println(data.getSize());
+
+        System.out.println("--------------");
+        data.set(5, 7);
+        System.out.println(data.get(5));
+        System.out.println(data.toString());
     }
 }
